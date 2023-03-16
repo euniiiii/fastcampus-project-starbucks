d20a001f5489c4b673f48e756b1fd96280f6048f
euniiiii <hee9920@naver.com>
2023-03-16 10:02:43 +0900

    lecture #26

--- js/main.js
+++ js/main.js
@@ -39,3 +39,57 @@ fadeEls.forEach(function(fadeEl, index) {
     opacity: 1
   });
 });
+
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true,
+});
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function() {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    promotionEl.classList.add('hide');
+  } else {
+    promotionEl.classList.remove('hide');
+  }
+});
+
+// 범위 랜덤 함수 (소수점 2자리까지)
+function random(min, max) {
+  return parseFloat((Math.random() * (max - min) + min))
+}
+
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut,
+    delay: random(0, delay)
+  })
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating1', .5, 15);
+floatingObject('.floating1', 1.5, 20);
